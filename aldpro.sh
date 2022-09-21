83b40539b2f75ecb8e5e22e81561cf551f973831
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-09-21 21:06:40 +0300

    Update aldpro.sh

--- aldpro.sh
+++ aldpro.sh
@@ -5,7 +5,7 @@ sudo apt-get install ca-certificates apt-transport-https
 sudo echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/ repository-base 1.7_x86-64 main non-free contrib" | sudo tee -a /etc/apt/sources.list
 sudo echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/ repository-extended 1.7_x86-64 main contrib non-free" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update && sudo apt-get upgrade -y
-echo -e "deb https://download.astralinux.ru/aldpro/stable/repository-main/ 1.1.0 main" | sudo tee -a /etc/apt/sources.list
+echo -e "deb https://download.astralinux.ru/aldpro/stable/repository-main/ 1.0.0 main" | sudo tee -a /etc/apt/sources.list
 echo -e "deb https://download.astralinux.ru/aldpro/stable/repository-extended/ generic main" | sudo tee -a /etc/apt/sources.list
 sudo touch /etc/apt/preferences.d/aldpro
 sudo echo -e "Package: *" | sudo tee -a /etc/apt/preferences.d/aldpro
