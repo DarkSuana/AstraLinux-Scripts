bc1135a381887eaddd5613692e3180282cecf6cf
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-09-22 23:05:50 +0300

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -4,6 +4,8 @@ apt-get install iptables openssh-server ca-certificates curl gnupg lsb-release -
 echo -e "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
 systemctl restart ssh
 echo -e "post-up iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE" | tee -a /etc/network/interfaces
+net.ipv4.ip_forward=1
+sysctl -p
 systemctl restart networking
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
