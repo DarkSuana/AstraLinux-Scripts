3fd7553dc535d17d0cfb4974220c6df4b713a41d
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-09-22 22:09:22 +0300

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -1,6 +1,6 @@
 hostnamectl set-hostname prod
 apt-get update && apt-get upgrade -y
-apt-get install iptables openssh-server ca-certificates curl gnupg lsb-release-y
+apt-get install iptables openssh-server ca-certificates curl gnupg lsb-release -y
 echo -e "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
 systemctl restart ssh
 echo -e "post-up iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE" | tee -a /etc/network/interfaces
