16ce8deb7a8113ad36150cac91cc6036cb5070ed
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-09-26 11:06:20 +0300

    Update aldpro.sh

--- aldpro.sh
+++ aldpro.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "Enter domain: $DOMAIN"
+echo "Enter name of DC: $DC"
+echo "Enter password: $PASS"
+echo "Enter local ip of your DC: $IP"
 sudo astra-modeswitch set 2
-sudo apt-get install ca-certificates apt-transport-https
+sudo apt-get install ca-certificates apt-transport-https vim -y
 sudo echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/repository-base 1.7_x86-64 main non-free contrib" | sudo tee -a /etc/apt/sources.list
 sudo echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/repository-extended 1.7_x86-64 main contrib non-free" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update && sudo apt-get upgrade -y
@@ -11,7 +15,7 @@ sudo echo -e "Package: *" | sudo tee -a /etc/apt/preferences.d/aldpro
 sudo echo -e "Pin: release n=generic" | sudo tee -a /etc/apt/preferences.d/aldpro
 sudo echo -e "Pin-Priority: 900" | sudo tee -a /etc/apt/preferences.d/aldpro
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install vim -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y aldpro-mp
 sudo apt-get install --fix-missing
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y aldpro-mp
+sudo /opt/rbta/aldpro/mp/bin/aldpro-server-install.sh -d $DOMAIN -n $DC -p $PASS --ip $IP --no-reboot 
