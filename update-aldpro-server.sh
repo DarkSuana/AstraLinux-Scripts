be9b4a28e3847c46de0d4adeaa2f8afe044c424f
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-09-26 14:06:57 +0300

    Create update-aldpro-server.sh

---
+++ update-aldpro-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This script updates the aldpro server from 1.0.0 to version 1.1.0
+sudo apt-get update && sudo apt-get upgrade -y
+sudo echo -e "deb https://download.astralinux.ru/aldpro/stable/repository-main/ 1.1.0 main " | sudo tee /etc/apt/sources.list.d/aldpro.list
+sudo echo -e "deb https://download.astralinux.ru/aldpro/stable/repository-extended/ generic main" | sudo tee /etc/apt/sources.list.d/aldpro.list
+sudo apt-get update && sudo apt-get dist-upgrade -y
+sudo systemctl enable dirsrv-watcher.timer
+sudo systemctl enable dirsrv-watcher.service
+sudo systemctl start dirsrv-watcher.timer
+sudo systemctl syaty dirsrv-watcher.service
+sudo reboot 
