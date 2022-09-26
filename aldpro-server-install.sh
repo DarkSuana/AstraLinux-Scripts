6e15baf5f293e0b16db870a034a5e3677a157e58
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-09-26 13:07:05 +0300

    Update aldpro-server-install.sh

--- aldpro-server-install.sh
+++ aldpro-server-install.sh
@@ -10,6 +10,7 @@ echo "Enter local ip of your DC: "
 read $IP
 sudo astra-modeswitch set 2
 sudo apt-get install ca-certificates apt-transport-https vim curl-y
+sudo echo -e "nameserver 127.0.0.1" | sudo tee -a /etc/resolv.conf
 sudo echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/repository-base 1.7_x86-64 main non-free contrib" | sudo tee -a /etc/apt/sources.list
 sudo echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/repository-extended 1.7_x86-64 main contrib non-free" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update && sudo apt-get upgrade -y
