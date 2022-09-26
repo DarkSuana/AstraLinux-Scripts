b2fe0ea01cec39367b905008745166fb6bf3bd2f
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-09-26 13:14:49 +0300

    Update and rename freeipa.sh to freeipa-server-install-setup.sh

--- freeipa.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install rng-tools -y
-sudo echo -e "HRNGDEVICE=/dev/urandom">> /etc/default/rng-tools-debian
-sudo apt-get install freeipa-*
