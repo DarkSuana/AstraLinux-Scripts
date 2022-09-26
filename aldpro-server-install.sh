0e7f89f1dde5f2362eaffff7ab177a452da8c804
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-09-26 14:12:48 +0300

    Update aldpro-server-install.sh

--- aldpro-server-install.sh
+++ aldpro-server-install.sh
@@ -12,8 +12,8 @@ read $IP
 sudo astra-modeswitch set 2
 sudo apt-get install ca-certificates apt-transport-https vim curl-y
 sudo echo -e "nameserver 127.0.0.1" | sudo tee -a /etc/resolv.conf
-sudo echo -e "deb http://download.astralinux.ru/astra/stable/1.7_x86-64/1.7.1/repository-base 1.7_x86-64 main non-free contrib" | sudo tee -a /etc/apt/sources.list
-sudo echo -e "deb http://download.astralinux.ru/astra/stable/1.7_x86-64/1.7.1/repository-extended 1.7_x86-64 main contrib non-free" | sudo tee -a /etc/apt/sources.list
+sudo echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/repository-base 1.7_x86-64 main non-free contrib" | sudo tee -a /etc/apt/sources.list
+sudo echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/repository-extended 1.7_x86-64 main contrib non-free" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update && sudo apt-get upgrade -y
 echo -e "deb https://download.astralinux.ru/aldpro/stable/repository-main/ 1.0.0 main" | sudo tee -a /etc/apt/sources.list.d/aldpro.list
 echo -e "deb https://download.astralinux.ru/aldpro/stable/repository-extended/ generic main" | sudo tee -a /etc/apt/sources.list.d/aldpro.list
