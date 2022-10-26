cfb7f478867951b81f8757fefe147c2722ea41ec
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-10-26 22:34:12 +0300

    Create aldpro-install.sh

---
+++ aldpro-install.sh
@@ -0,0 +1,14 @@
+astra-modeswitch set 2
+apt-get install ca-certificates apt-transport-https -y
+echo -e "nameserver 127.0.0.1" | sudo tee -a /etc/resolv.conf
+echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/repository-base 1.7_x86-64 main non-free contrib" | sudo tee -a /etc/apt/sources.list
+echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/repository-extended 1.7_x86-64 main contrib non-free" | sudo tee -a /etc/apt/sources.list
+apt-get update && apt-get install vim curl wget -y
+echo -e "deb https://download.astralinux.ru/aldpro/stable/repository-main/ 1.0.0 main" | sudo tee -a /etc/apt/sources.list.d/aldpro.list
+echo-e "deb https://download.astralinux.ru/aldpro/stable/repository-extended/ generic main" | sudo tee -a /etc/apt/sources.list.d/aldpro.list
+touch /etc/apt/preferences.d/aldpro
+echo -e "Package: *" | sudo tee -a /etc/apt/preferences.d/aldpro
+echo -e "Pin: release n=generic" | sudo tee -a /etc/apt/preferences.d/aldpro
+echo -e "Pin-Priority: 900" | sudo tee -a /etc/apt/preferences.d/aldpro
+apt-get update && sudo apt-get upgrade -y
+DEBIAN_FRONTEND=noninteractive apt-get install -q -y aldpro-mp
