9ae95392fc6d8a1258e8bf04f97ff73cd597b2b4
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-11-14 15:49:39 +0300

    Update aldpro-install.sh

--- aldpro-install.sh
+++ aldpro-install.sh
@@ -1,14 +1,18 @@
-sudo astra-modeswitch set 2
-sudo apt-get install ca-certificates apt-transport-https -y
+#!/bin/bash
+# Use only ifupdown, not NetworkManager
+# Use FQDN to your hostname (like dc1.test.local)
+# Change in /etc/hosts 127.0.1.1 on your FQDN name with short name
+astra-modeswitch set 2
+apt-get install ca-certificates apt-transport-https -y
 echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/repository-base 1.7_x86-64 main non-free contrib" | sudo tee -a /etc/apt/sources.list
 echo -e "deb http://download.astralinux.ru/astra/frozen/1.7_x86-64/1.7.1/repository-extended 1.7_x86-64 main contrib non-free" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update && sudo apt-get upgrade -y
+apt-get update && sudo apt-get upgrade -y
 apt-get install vim curl wget -y
 echo -e "deb https://download.astralinux.ru/aldpro/stable/repository-main/ 1.1.2 main" | sudo tee -a /etc/apt/sources.list.d/aldpro.list
 echo -e "deb https://download.astralinux.ru/aldpro/stable/repository-extended/ generic main" | sudo tee -a /etc/apt/sources.list.d/aldpro.list
-sudo touch /etc/apt/preferences.d/aldpro
+touch /etc/apt/preferences.d/aldpro
 echo -e "Package: *" | sudo tee -a /etc/apt/preferences.d/aldpro
 echo -e "Pin: release n=generic" | sudo tee -a /etc/apt/preferences.d/aldpro
 echo -e "Pin-Priority: 900" | sudo tee -a /etc/apt/preferences.d/aldpro
 sudo apt-get update && sudo apt-get upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y aldpro-mp
+DEBIAN_FRONTEND=noninteractive apt-get install -q -y aldpro-mp
