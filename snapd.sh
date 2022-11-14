83799dfe1ad1cb061c6c59b175d505a9e97da0dc
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2022-11-14 15:20:54 +0300

    Update snapd.sh

--- snapd.sh
+++ snapd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#This script installing Snap Store (snapd)
+#This script adds Debian repositories
 sudo apt-get install debian-archive-keyring
 sudo apt-get install gpg
 echo -e "deb [trusted=yes] https://mirror.yandex.ru/debian/ buster main contrib non-free" | sudo tee -a /etc/apt/sources.list
@@ -9,5 +9,3 @@ gpg --keyserver keyserver.ubuntu.com --recv-key 0E98404D386FA1D9
 gpg -a --export 0E98404D386FA1D9 | sudo apt-key add -
 gpg --keyserver keyserver.ubuntu.com --recv-key DCC9EFBF77E11517
 gpg -a --export DCC9EFBF77E11517 | sudo apt-key add -
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install snapd -y
